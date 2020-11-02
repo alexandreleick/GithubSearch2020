1afa0fff3d91251743a84cb10b6fe953d699bcb4
Cyril Colinet <cyrilcolinet.pro@gmail.com>
2020-11-02 18:50:50 +0100

    fix(name): Name fix

--- src/screens/FavouriteScreen/FavouriteScreen.component.tsx
+++ src/screens/FavouriteScreen/FavouriteScreen.component.tsx
@@ -4,7 +4,7 @@ import { Layout, Text } from '@ui-kitten/components'
 const FavouriteScreen: React.FC = () => {
   return (
     <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F4F4F4' }}>
-      <Text category="h1">USERS</Text>
+      <Text category="h1">FAVOURITES</Text>
     </Layout>
   )
 }
