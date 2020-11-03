2e552bcfc8a4cde07195db730ad7288b3e67e618
Thibault FOUCOU <thibault.foucou@epitech.eu>
2020-11-03 18:03:29 +0100

    [+] added beginning of search

--- src/screens/Search/SearchScreen/SearchScreen.component.tsx
+++ src/screens/Search/SearchScreen/SearchScreen.component.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Input, Layout, Text, Button} from '@ui-kitten/components'
 import {components} from "@eva-design/eva/mapping";
 import {View} from "react-native";
+import useUserSearch from "../../../hooks/user/useUserSearch.hook";
 
 
 const SearchScreen: React.FC = () => {
     const [value, setValue] = React.useState('');
+
+    const {
+        data,
+        loading,
+        error,
+        dispatchRequest,
+    } = useUserSearch()
+    useEffect(() => {
+        console.log(data)
+    })
     const onChangeSearch = (query: string) => setValue(query)
+
+
+    const onSubmit = () => {
+        dispatchRequest(value)
+    }
 
     return (
         <>
@@ -17,7 +33,15 @@ const SearchScreen: React.FC = () => {
                     onChangeText={onChangeSearch}
                     style={{flexGrow: 1}}
                 />
-                <Button style={{position: 'absolute', right: 0, top: 0, height: 9, backgroundColor: 'grey', borderColor: 'grey'}}>
+                <Button style={{
+                    position: 'absolute',
+                    right: 0,
+                    top: 0,
+                    height: 9,
+                    backgroundColor: 'black',
+                    borderColor: 'black'
+                }}
+                        onPress={onSubmit}>
                     Search
                 </Button>
             </View>
