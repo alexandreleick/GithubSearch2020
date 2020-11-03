407d6fc8ac5975ec15315d693fe88703baf103ac
Thibault FOUCOU <thibault.foucou@epitech.eu>
2020-11-03 15:07:59 +0100

    [+] added beginning of search

--- src/screens/Search/SearchScreen/SearchScreen.component.tsx
+++ src/screens/Search/SearchScreen/SearchScreen.component.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 import {Input, Layout, Text, Button} from '@ui-kitten/components'
 import {components} from "@eva-design/eva/mapping";
 import {View} from "react-native";
-import {grey50} from "react-native-paper/lib/typescript/src/styles/colors";
 
 
 const SearchScreen: React.FC = () => {
     const [value, setValue] = React.useState('');
+    const onChangeSearch = (query: string) => setValue(query)
 
     return (
         <>
             <View style={{flexDirection: 'row'}}>
                 <Input
-                    placeholder='Place your Text'
+                    placeholder='Search for users / repositories'
                     value={value}
-                    onChangeText={nextValue => setValue(nextValue)}
+                    onChangeText={onChangeSearch}
                     style={{flexGrow: 1}}
                 />
                 <Button style={{position: 'absolute', right: 0, top: 0, height: 9, backgroundColor: 'grey', borderColor: 'grey'}}>
