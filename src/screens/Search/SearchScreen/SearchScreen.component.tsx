5a702a02f3c00118e7b2ef29cd8de6a6c68582c8
Tristan Morera <tristan.morera@icloud.com>
2020-11-03 14:53:01 +0100

    [+] started search bar

--- src/screens/Search/SearchScreen/SearchScreen.component.tsx
+++ src/screens/Search/SearchScreen/SearchScreen.component.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import { Layout, Text } from '@ui-kitten/components'
+import {Input, Layout, Text, Button} from '@ui-kitten/components'
+import {components} from "@eva-design/eva/mapping";
+import {View} from "react-native";
+import {grey50} from "react-native-paper/lib/typescript/src/styles/colors";
+
 
 const SearchScreen: React.FC = () => {
-  return (
-    <Layout level="2" style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text category="h1">SEARCH</Text>
-    </Layout>
-  )
+    const [value, setValue] = React.useState('');
+
+    return (
+        <>
+            <View style={{flexDirection: 'row'}}>
+                <Input
+                    placeholder='Place your Text'
+                    value={value}
+                    onChangeText={nextValue => setValue(nextValue)}
+                    style={{flexGrow: 1}}
+                />
+                <Button style={{position: 'absolute', right: 0, top: 0, height: 9, backgroundColor: 'grey', borderColor: 'grey'}}>
+                    Search
+                </Button>
+            </View>
+        </>
+    );
 }
 
 export default SearchScreen
