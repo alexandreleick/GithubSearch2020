c682073a304503be701da6f68a4cb6c321ffd10f
Tristan Morera <tristan.morera@icloud.com>
2020-11-05 22:07:26 +0100

    [+] Starting to make repository request

--- src/screens/Favourites/FavouriteScreen/FavouriteScreen.component.tsx
+++ src/screens/Favourites/FavouriteScreen/FavouriteScreen.component.tsx
@@ -1,23 +1,36 @@
 import React from 'react'
-import { TextInput } from 'react-native'
-import { Layout, Text, Button, Avatar, ListItem } from '@ui-kitten/components'
+import {TextInput, Animated, View} from 'react-native'
+import { Layout, Text, Avatar, Button, ListItem } from '@ui-kitten/components'
 
 const Favourites = () => <Button size="tiny">Delete</Button>
 
 const ItemImage = () => <Avatar source={require('../../../assets/icon/icon.png')} />
 
-const FavouriteScreen: React.FC = () => {
-  return (
-    <>
-      <Button>Add an item</Button>
-      <ListItem
-        title="Item 1"
-        description="A set of React Native components"
-        accessoryLeft={ItemImage}
-        accessoryRight={Favourites}
-      />
-    </>
-  )
+const FavouriteScreen = () => {
+    return (
+        <>
+            <ListItem
+                title="Item"
+                description="A set of React Native components"
+                accessoryLeft={ItemImage}
+                accessoryRight={Favourites}
+            />
+        </>
+    )
 }
 
-export default FavouriteScreen
+const AddItem: React.FC = () => {
+    return(
+        <>
+            <View>
+                <Button
+                    title={"AddAnItem"}
+                    OnClick={{FavouriteScreen}}>
+                    Add an item
+                </Button>
+            </View>
+        </>
+    );
+}
+
+export default AddItem
