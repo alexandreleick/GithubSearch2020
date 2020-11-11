01a2a9b6f018db4ca45d6d2a3a9d89f73314e552
Tristan Morera <tristan.morera@icloud.com>
2020-11-11 14:40:29 +0100

    [+] Added front header repository display information

--- src/components/Repository/RepositoryHeader/RepositoryHeader.component.tsx
+++ src/components/Repository/RepositoryHeader/RepositoryHeader.component.tsx
@@ -1,7 +1,7 @@
 import { Repository } from '../../../types/repositories/repository.type'
 import React from 'react'
 import { Text } from 'react-native'
-import { RepositoryCard } from './RepositoryHeader.styled'
+import { RepositoryCard, RepoName, RepoPart, LeftRepoPart, StatDescription, StatValue, Stat, StatisticsPart } from './RepositoryHeader.styled'
 import { Avatar } from '@ui-kitten/components'
 
 type RepositoryHeaderProps = {
@@ -12,25 +12,52 @@ const RepositoryHeader: React.FC<RepositoryHeaderProps> = (props: RepositoryHead
   const { repo } = props
 
   const isPrivate = (is_private: boolean) => {
-    if (!is_private) return 'Non'
-    return 'Oui'
+    if (!is_private) return 'This repository is public'
+    return 'This repository is private'
   }
 
   const isFork = (is_fork: boolean) => {
-    if (!is_fork) return 'Non'
-    return 'Oui'
+    if (!is_fork) return 'This is not a Fork'
+    return 'This is a Fork'
   }
 
-  return (
-    <RepositoryCard>
-      <Text>{repo.name}</Text>
-      <Text>{repo.size}</Text>
-      <Text>{isPrivate(repo.private)}</Text>
-      <Text>{repo.description}</Text>
-      <Text>{isFork(repo.private)}</Text>
-      <Text>{repo.default_branch}</Text>
-    </RepositoryCard>
-  )
+    return (
+        <RepositoryCard>
+            <RepoPart>
+                <LeftRepoPart>
+                    <RepoName>{repo.name}</RepoName>
+                </LeftRepoPart>
+            </RepoPart>
+            <StatisticsPart>
+                <Stat>
+                    <StatDescription>Fork</StatDescription>
+                    <StatValue>{isFork(repo.private)}</StatValue>
+                </Stat>
+            </StatisticsPart>
+            <StatisticsPart>
+                <Stat>
+                    <StatDescription>Private / Public</StatDescription>
+                    <StatValue>{isPrivate(repo.private)}</StatValue>
+                </Stat>
+            </StatisticsPart>
+            <StatisticsPart>
+                <Stat>
+                    <StatDescription>Size</StatDescription>
+                    <StatValue>{repo.size} MB</StatValue>
+                </Stat>
+            </StatisticsPart>
+            <StatisticsPart>
+                <Stat>
+                    <StatDescription>Branch</StatDescription>
+                    <StatValue>{repo.default_branch}</StatValue>
+                </Stat>
+            </StatisticsPart>
+            <Stat style={{ marginTop: 10 }}>
+                <StatDescription>Description</StatDescription>
+                <StatValue>{repo.description}</StatValue>
+            </Stat>
+        </RepositoryCard>
+    )
 }
 
 export default RepositoryHeader
