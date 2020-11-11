e9396ab89badb7761d79d0e8f28922e02cfcbfd0
Tristan Morera <tristan.morera@icloud.com>
2020-11-11 10:46:46 +0100

    [+] added hook for repoRenderer

---
+++ src/hooks/repositories/useGetRepo.hook.ts
@@ -0,0 +1,24 @@
+import useAxios from 'axios-hooks'
+import { Repository } from '../../types/repositories/repository.type'
+
+type UseGetRepoType = () => {
+    data: Repository
+    loading: boolean
+    error: any
+    dispatchGetRepo: (RepoUrl: string) => Promise<any>
+}
+
+const useGetRepo: UseGetRepoType = () => {
+    const [{ data, loading, error }, execute] = useAxios<Repository>({}, { manual: true })
+
+    const dispatchGetRepo = (RepoUrl: string) => execute({ url: RepoUrl })
+
+    return {
+        data,
+        loading,
+        error,
+        dispatchGetRepo,
+    }
+}
+
+export default useGetRepo
