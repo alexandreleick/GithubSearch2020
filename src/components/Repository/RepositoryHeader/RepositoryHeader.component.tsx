dd88d8322c77ac1c74212434ab762b884cc0d9a6
Thibault FOUCOU <thibault.foucou@epitech.eu>
2020-11-11 14:23:21 +0100

    [+] added repository header information

--- src/components/Repository/RepositoryHeader/RepositoryHeader.component.tsx
+++ src/components/Repository/RepositoryHeader/RepositoryHeader.component.tsx
@@ -11,9 +11,24 @@ type RepositoryHeaderProps = {
 const RepositoryHeader: React.FC<RepositoryHeaderProps> = (props: RepositoryHeaderProps) => {
   const { repo } = props
 
+  const isPrivate = (is_private: boolean) => {
+    if (!is_private) return 'Non'
+    return 'Oui'
+  }
+
+  const isFork = (is_fork: boolean) => {
+    if (!is_fork) return 'Non'
+    return 'Oui'
+  }
+
   return (
     <RepositoryCard>
-      <Avatar source={{ uri: repo.owner.avatar_url, cache: 'force-cache' }} />
+      <Text>{repo.name}</Text>
+      <Text>{repo.size}</Text>
+      <Text>{isPrivate(repo.private)}</Text>
+      <Text>{repo.description}</Text>
+      <Text>{isFork(repo.private)}</Text>
+      <Text>{repo.default_branch}</Text>
     </RepositoryCard>
   )
 }
